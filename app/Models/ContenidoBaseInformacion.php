13281ce5f3f148a33dba681cfadbecc6453b3501
Karen Laguna <kharenlaguna@gmail.com>
2021-02-09 12:36:34 -0500

    Creación de Modelos

---
+++ app/Models/ContenidoBaseInformacion.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ContenidoBaseInformacion extends Model
+{
+    use HasFactory;
+
+    protected $table = 'contenido_base_informacion';
+
+    protected $fillable = [
+        'llave_cruce',
+        'campo_informacion',
+        'grupo_categoria',
+        'categoria_especifica',
+    ];
+}
